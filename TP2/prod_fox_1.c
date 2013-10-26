a91de09d2127815e0776e1c5e890e461f27f64f3
Hartorn <bazirehoussin@gmail.com>
2013-10-26 15:44:46 +0200

    nettoyage du code

--- TP2/prod_fox_1.c
+++ TP2/prod_fox_1.c
@@ -75,10 +75,13 @@ int main( int argc, char **argv ) {
 	
 	int periods[2] = {1,1};
 	
+	//initialisation de la topologie cartesienne, recuperation de la taille, coordonnees et rang
 	MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods,1, &world); 
 	MPI_Comm_rank( world, &myrank ); 
 	MPI_Comm_size( world, &size);
+	MPI_Cart_coords(world, myrank, 2, coords);
 	
+	//creation des matrices locales necessaires au calcul
 	int * C = calloc(taille_bloc*taille_bloc, sizeof(int));
 	int * A = calloc(taille_bloc*taille_bloc, sizeof(int));
 	int * B = calloc(taille_bloc*taille_bloc, sizeof(int));
@@ -89,13 +92,13 @@ int main( int argc, char **argv ) {
 
 	MPI_Datatype bloc;
 	
+	//creation d un type pour decrire un bloc
 	MPI_Type_vector(taille_bloc, taille_bloc, n, MPI_INT, &bloc);
-	MPI_Type_create_resized(bloc,0,sizeof(int),&bloc);	
+	MPI_Type_create_resized(bloc,0,sizeof(int),&bloc);	//permet d utiliser Scatterv et Gatherv
 	MPI_Type_commit(&bloc); 
 
-
 	int *A_final, *B_final, *C_final;
-	
+	//initialisation propre au proc 0
 	if(myrank == 0)
 	{
 		printf("n: %d\nnb: %d\ntaille:%d\n",n,nb_blocs_ligne,taille_bloc);
@@ -117,8 +120,7 @@ int main( int argc, char **argv ) {
 				}
 			}
 		}
-		
-	
+	//creation des tableaux necessaires pour Scatterv et GatherV
 	sendcnts = calloc(nb_blocs, sizeof(int));
 	displs = calloc(nb_blocs, sizeof(int));
 	
@@ -137,33 +139,25 @@ int main( int argc, char **argv ) {
 		affiche_matrice(B_final,taille_bloc);
 		printf("\n\n");
 	}
-		
-	//~ MPI_Scatter(A_final,1, bloc, A, taille_bloc*taille_bloc, MPI_INT, 0, world);
+	//envoi des donnees aux autres processus
 	MPI_Scatterv(A_final, sendcnts, displs,bloc, A, taille_bloc*taille_bloc, MPI_INT, 0, world);
 	
-	
-	//initialisation
-	MPI_Cart_coords(world, myrank, 2, coords);
-	
-		if(myrank == 3){
+	if(myrank == 3){
 			printf("coords i : %d, j : %d\n", coords[0], coords[1]);
 			affiche_matrice(A,taille_bloc);
 	printf("\n\n");
 	}
 	
+	//creation d un communicateur sur la ligne
 	MPI_Comm ligne;
 	MPI_Comm_split(world, coords[0], 0, &ligne);
-	
+	MPI_Comm_size( ligne, &size_ligne);
+	MPI_Comm_rank( ligne, &rank_ligne);
+
 	//inutile
 	//~ MPI_Comm colonne;
 	//~ MPI_Comm_split(world, coords[1], 0, &colonne);
-
-	MPI_Comm_rank( ligne, &rank_ligne);
-	//inutile
 	//~ MPI_Comm_rank( colonne, &rank_colonne ); 
-	
-	MPI_Comm_size( ligne, &size_ligne);
-	//inutile
 	//~ MPI_Comm_size( colonne, &size_colonne ); 
 	
 	for (k = 0; k<n ;k++)
@@ -183,11 +177,13 @@ int main( int argc, char **argv ) {
 		MPI_Bcast(A_bis, taille_bloc*taille_bloc, MPI_INT, MPI_Cart_rank(world, tmp_coords, &tmp_rank), ligne);
 
 		produit_matriciel(C,A_bis, B, taille_bloc);	
-
+		
+		//rotation circulaire sur les colonnes
 		MPI_Cart_shift(world, 1, 1, &rank_source, &rank_dest);
 		MPI_Sendrecv_replace(B, taille_bloc*taille_bloc, MPI_INT,rank_dest, 1, rank_source, MPI_ANY_TAG, world, &status);
 	}
 	
+	//reconstruction de la matrice C final
 	MPI_Gatherv(C, taille_bloc*taille_bloc, MPI_INT, C_final, sendcnts, displs, bloc, 0, world);
 
 	MPI_Type_free(&bloc);
@@ -206,5 +202,6 @@ int main( int argc, char **argv ) {
 		free(B_final);
 		free(C_final);	
 	}
+	
   return EXIT_SUCCESS;
 } 
