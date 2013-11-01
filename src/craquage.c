423a59afce4217e536809229b659a66716951de5
Hartorn <bazirehoussin@gmail.com>
2013-11-01 20:56:36 +0100

    test

--- src/craquage.c
+++ src/craquage.c
@@ -18,24 +18,6 @@ int main (int argc, char* argv[])
 		printf("usage : ./craquage p r m\n");
 		return EXIT_FAILURE;
 	}
-	       
-        /*int longueur_max = 0;
-	unsigned long int ulmax = ULONG_MAX;
-      	ulmax /=15;
-        ulmax *=14;
-        ulmax ++;
-        while(ulmax>15)
-          {
-            ulmax/=15;
-            longueur_max++;
-          }
-
-	if(atoi(argv[2])>longueur_max)
-	  {
-	    printf("Longueur max du mot de passe trop grande : %d au lieu de %d\n", atoi(argv[2]), longueur_max);
-	    return EXIT_FAILURE;
-	  }
-	*/
 
 	// Initialisation des variables
 	int nb_esclaves = atoi(argv[1]);
@@ -63,7 +45,25 @@ int main (int argc, char* argv[])
 	int bufid, info, bytes, type, source;
 	char * solution;
 
-	pvm_catchout(stderr);
+	//~ pvm_catchout(stderr);
+	
+	int longueur_max = 0;
+	unsigned long int ulmax = ULONG_MAX;
+      	ulmax /=15;
+        ulmax *=14;
+        ulmax ++;
+        while(ulmax>15)
+          {
+            ulmax/=15;
+            longueur_max++;
+          }
+
+	if(atoi(argv[2])>longueur_max)
+	  {
+	    printf("Longueur max du mot de passe trop grande : %d au lieu de %d\n", atoi(argv[2]), longueur_max);
+	    return EXIT_FAILURE;
+	  }
+	
 	//initialisation des esclaves
 	//printf("chemin:%s argv_esclave[0]:%s argv_esclave[1]:%s nb_esclaves:%d \n", chemin,argv_esclave[0], argv_esclave[1], nb_esclaves); 
 	pvm_spawn(chemin, argv_esclave, PvmTaskDefault,"", nb_esclaves, tids);
