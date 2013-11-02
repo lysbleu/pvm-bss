c858d582db03d1afbf34d3ef16044a911bcb1399
HOUSSIN Bazire <bhoussin@enseirb-matmeca.fr>
2013-11-02 18:58:43 +0100

    version grand entier

--- src/craquage.c
+++ src/craquage.c
@@ -7,10 +7,12 @@
 #include <unistd.h>
 #include <limits.h>
 #include<sys/time.h>
+#include "gmp/gmp.h"
 
 #define TAILLE_MAX  1000
 #define NB_TRANCHES 10
 #define MIN(n1,n2) n1 < n2 ? n1 : n2
+#define MAX(n1,n2) n1 > n2 ? n1 : n2
 
 int main (int argc, char* argv[])
 {
@@ -26,58 +28,68 @@ int main (int argc, char* argv[])
 	int longueur_mdp = atoi(argv[2]);
 	char* mdp = (char*) calloc(strlen(argv[3])+1, sizeof(char));
 	strcpy(mdp, argv[3]);
- 
+
+	/*char *debut_sequence, *pas_reel, *pas, *fin_exec;
+	debut_sequence = calloc(1, sizeof(char)*((4*longueur_mdp+1)/3 +1));
+	pas_reel = calloc(1, sizeof(char)*((4*longueur_mdp+1)/3 +1));
+	pas = calloc(1, sizeof(char)*((4*longueur_mdp+1)/3 +1));
+	fin_exec = calloc(1, sizeof(char)*((4*longueur_mdp+1)/3 +1));
+	*/
+	//declaration de type de tres long entiers (avec bibliotheque GMP)
+	mpz_t debut_sequence, pas_reel, pas, fin_exec;
+	//mpz_inits(debut_sequence, pas_reel, pas, fin_exec, NULL);
+	mpz_init(debut_sequence);
+	mpz_init(pas_reel);
+	mpz_init(pas);
+	mpz_init(fin_exec);
 	//recuperation du chemin de l executable
 	char* chemin = getcwd(NULL, 1000);
 	strcat(chemin, "/craquage_esclave");
-	//printf("%s\n", chemin);
+	printf("%s\n", chemin);
+
 	//creation des arguments pour l esclave
-	char *argv_esclave[2] ;
+	char *argv_esclave[3];
+	argv_esclave[2]=NULL;
 	argv_esclave[0] = (char*) calloc(strlen(argv[2])+1, sizeof(char));
 	strcpy(argv_esclave[0],argv[2]);
 	 
 	argv_esclave[1] = (char*) calloc(strlen(argv[3])+1, sizeof(char));
 	strcpy(argv_esclave[1],argv[3]);
-	 
+	printf("strlen %d, %d\n", strlen(argv[2]), strlen(argv[3]));
+	printf("nb_esclaves %d\n", nb_esclaves);
 	int i;
 	int trouve = 0;
 	int fini = 0;
-	unsigned long int debut_sequence, pas_reel, pas, fin_exec;
+	int size;
+	char* envoi_char;
+	//unsigned long int debut_sequence, pas_reel, pas, fin_exec;
+	
 	int bufid, info, bytes, type, source;
 	char * solution;
-
-	//pvm_catchout(stderr);
-	
-	/*int longueur_max = 0;
-	unsigned long int ulmax = ULONG_MAX;
-      	ulmax /=15;
-        ulmax *=14;
-        ulmax ++;
-        while(ulmax>15)
-          {
-            ulmax/=15;
-            longueur_max++;
-          }
-
-	if(atoi(argv[2])>longueur_max)
-	  {
-	    printf("Longueur max du mot de passe trop grande : %d au lieu de %d\n", atoi(argv[2]), longueur_max);
-	    return EXIT_FAILURE;
-	  }
-       */
-	//initialisation des esclaves
-	//printf("chemin:%s argv_esclave[0]:%s argv_esclave[1]:%s nb_esclaves:%d \n", chemin,argv_esclave[0], argv_esclave[1], nb_esclaves); 
+	pvm_catchout(stderr);
 	struct timeval tv1, tv2;
 	gettimeofday(&tv1, NULL);
-
 	pvm_spawn(chemin, argv_esclave, PvmTaskDefault,"", nb_esclaves, tids);
 
 	//calcul du pas, fin_exec (= fin execution)
-	debut_sequence = 0;
-	fin_exec = ((pow(15, longueur_mdp)-1)*15)/14;
-	pas = fin_exec/(nb_esclaves*longueur_mdp);
+	mpz_set_ui(debut_sequence, 0);
+	mpz_ui_pow_ui(fin_exec, 15, longueur_mdp+1);	
+	mpz_sub_ui(fin_exec, fin_exec, 15);
+	mpz_cdiv_q_ui(fin_exec, fin_exec, 14);
+	
+	mpz_set(pas, fin_exec);
+	mpz_cdiv_q_ui(pas, pas, nb_esclaves*longueur_mdp);
+	if(mpz_cmp_ui(pas, 0)==0)
+	  {
+	    mpz_set_ui(pas,1); 
+	  }
 
-	//printf("fin_exec: %lu, pas:%lu\n", fin_exec, pas); 
+	gmp_printf("fin_exec: %Zd\npas:%Zd\ndebut_sequence:%Zd\n",fin_exec, pas, debut_sequence);
+	//debut_sequence = 0;
+	//fin_exec = ((pow(15, longueur_mdp)-1)*15)/14;
+	//pas = MAX(*fin_exec/(nb_esclaves*longueur_mdp), 1);
+
+	//printf("debut_sequence fin_exec: %lu, pas:%lu\n", fin_exec, pas); 
 	//boucle principale
 	while(!trouve && fini!=nb_esclaves)
 	{
@@ -97,32 +109,46 @@ int main (int argc, char* argv[])
 			case(0)://mot de passe trouve
 			solution = calloc(bytes, sizeof(char));
 			pvm_upkstr(solution);
-			//printf("\nLa solution est : %s\n\n", solution);
+			printf("\nLa solution est : %s\n\n", solution);
 			trouve = 1;
 			break;	
 			
 			case(1)://esclave veut plus de donnees
-			if(debut_sequence < fin_exec){
-				pas_reel = MIN(pas, fin_exec - debut_sequence);
+			  if(mpz_cmp(debut_sequence, fin_exec)< 0){
+			    
+			    mpz_sub(pas_reel, fin_exec, debut_sequence);
+			    if(mpz_cmp(pas, pas_reel)<0)
+			      {
+				mpz_set(pas_reel, pas);
+			      }
+			        //*pas_reel = MIN(*pas, *fin_exec - *debut_sequence);
 				//prendre en compte la fin des donnees dans le calcul du pas
 				//envoi des donnes a l esclave
 				pvm_initsend(PvmDataDefault);
-				pvm_pkulong(&debut_sequence, 1, 1);
-				pvm_pkulong(&pas_reel, 1, 1);
+				size = gmp_asprintf(&envoi_char, "%Zd", debut_sequence);
+				pvm_pkint(&size, 1, 1);
+				pvm_pkbyte(envoi_char,size+1, 1);
+				free(envoi_char);
+				
+				size = gmp_asprintf(&envoi_char, "%Zd", pas_reel);
+			      	pvm_pkint(&size, 1, 1);
+				pvm_pkbyte(envoi_char,size+1 , 1);
+				//pvm_pkulong(&debut_sequence, 1, 1);
+				//pvm_pkulong(&pas_reel, 1, 1);
 				pvm_send(source,0);
 				
-				if(pas_reel != pas)
+				if(mpz_cmp(pas_reel,pas)!=0)
 				  {
-				    debut_sequence = fin_exec;
+				    mpz_set(debut_sequence,fin_exec);
 				  }
 				else
 				  {
-				    debut_sequence += pas;
+				    mpz_add(debut_sequence, debut_sequence,pas);
 				  }
 				}
 			else{
 				fini++ ;
-				//printf("Pas de solution pour %d esclave(s)\n", fini);
+				printf("Pas de solution pour %d esclave(s)\n", fini);
 			}		
 			
 			break;
@@ -139,8 +165,13 @@ int main (int argc, char* argv[])
 	}
 	
 	pvm_exit();
-	gettimeofday(&tv2, NULL);
-	printf("%d %ld\n",longueur_mdp,(tv2.tv_sec-tv1.tv_sec)*1000 + (tv2.tv_usec-tv1.tv_usec)/1000); 
+	//gettimeofday(&tv2, NULL);
+	//printf("%d %ld\n",longueur_mdp,(tv2.tv_sec-tv1.tv_sec)*1000 + (tv2.tv_usec-tv1.tv_usec)/1000); 
+	//mpz_clears(debut_sequence, pas_reel, pas, fin_exec, NULL);
+	mpz_clear(debut_sequence);
+	mpz_clear(pas_reel);
+	mpz_clear(pas);
+	mpz_clear(fin_exec);
 	free(tids);
 	free(mdp);
 	free(argv_esclave[0]);
--- src/data.sh
+++ src/data.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-for ((i=1;i<20;i++))
+for ((i=1;i<100;i+=2))
 do
  for ((j=1; j<9; j++))
  do
