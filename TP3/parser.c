49f9fda749bd2fc563bc53db419087e2aa6588ee
Hartorn <bazirehoussin@gmail.com>
2013-11-15 21:49:23 +0100

    correction du parser, ajout des tests

--- TP3/parser.c
+++ TP3/parser.c
@@ -5,29 +5,31 @@
 #include <math.h>
 
 #define NB_CHAR_LINE 50
-int parse(char* filename, Atome *initialDatas, int rank, int nbProcs) {
+int parse(char* filename, Atome **initialDatas, int rank, int nbProcs) {
     FILE *file = fopen(filename, "r");
     fseek(file, 0, SEEK_END);
-    long nbLines = (long) floor(ftell(file) / NB_CHAR_LINE);
+
+    long nbLines = (long) floor( (ftell(file)) / NB_CHAR_LINE);
     int nbLinesByProc = nbLines / nbProcs;
     int remain  = nbLines % nbProcs;
 
     long cursor = 0;
     if(rank < remain) {
         nbLinesByProc++;
-        cursor = rank * (nbLinesByProc + 1) * NB_CHAR_LINE;
+        cursor = rank * nbLinesByProc * NB_CHAR_LINE;
     }
     else {
         cursor = (rank * nbLinesByProc + remain) * NB_CHAR_LINE;
     }
-
-    char buffer[NB_CHAR_LINE];
+	char buf[51];
+	char *buf2;
     fseek(file, cursor, SEEK_SET);
-    initialDatas = calloc(nbLinesByProc, sizeof(Atome));
-    for (int i = 0; i < nbLinesByProc) {
-        fscanf(file, "%lf %lf %lf %lf %lf\n", initialDatas[i].m,\
-               initialDatas.pos[0], initialDatas.pos[1], \
-               initialDatas.vit[0], initialDatas.vit[1]);
+	*initialDatas = calloc(nbLinesByProc, sizeof(Atome));
+
+    for (int i = 0; i < nbLinesByProc; i++) {
+        fscanf(file, "%lf %lf %lf %lf %lf\n", &((*initialDatas)[i].m), 
+			&((*initialDatas)[i].pos[0]), &((*initialDatas)[i].pos[1]),
+			&((*initialDatas)[i].vit[0]), &((*initialDatas)[i].vit[1]));
     }
     fclose(file);
     return nbLinesByProc;
@@ -35,7 +37,7 @@ int parse(char* filename, Atome *initialDatas, int rank, int nbProcs) {
 
 void displayAtoms(Atome *atoms, int size) {
     for(int i = 0 ; i < size ; i++) {
-        printf("Masse : %lf, position : (%lf, %lf), vitesse : (%lf, %lf)\n",
+        printf("Masse : %le, position : (%le, %le), vitesse : (%le, %le)\n",
                atoms[i].m, atoms[i].pos[0], atoms[i].pos[1],
                atoms[i].vit[0], atoms[i].vit[1]);
     }
---
+++ TP3/test_parser/test_parse.c
@@ -0,0 +1,32 @@
+#include "../parser.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char** argv)
+{
+	Atome* at;
+	int taille = parse("data_test.txt", &at, 0, 5);
+	displayAtoms(at, taille);
+	free(at);
+	printf("\n");
+	taille = parse("data_test.txt", &at, 1, 5);
+	displayAtoms(at, taille);
+	free(at);
+	printf("\n");
+	taille = parse("data_test.txt", &at, 2, 5);
+	displayAtoms(at, taille);
+	free(at);
+	printf("\n");
+	taille = parse("data_test.txt", &at, 3, 5);
+	displayAtoms(at, taille);
+	free(at);
+	printf("\n");
+	taille = parse("data_test.txt", &at, 4, 5);
+	displayAtoms(at, taille);
+	free(at);
+	
+
+	
+	return EXIT_SUCCESS;
+}
