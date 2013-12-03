d350d1e1f2c72bf4a8043d32a6d7f6ce07b8f430
Hartorn <bazirehoussin@gmail.com>
2013-12-03 02:14:01 +0100

    script

--- TP3/perf.sh
+++ TP3/perf.sh
@@ -2,12 +2,12 @@
 rm perf/perf_*.txt
 for i in 48 40 30 20 10 1
 do
-	for j in 80 75 70 75 60 55 50 45 40 35 30 25 20 15 10 5 1
+	for j in 80 75 70 65 60 55 50 45 40 35 30 25 20 15 10 5 1
 	do
-		./genere_data $j
-		duree=`/usr/bin/time -f "%e" mpiexec -np $i ./interaction ./data/data_$j.txt 1000`
-		echo "$($j*$j) $duree" >> ./perf/perf_$i.txt 
-	    echo "$j iter\n"
+		#~ ./genere_data $j
+		#~ duree=`/usr/bin/time -f "%e" mpiexec -np $i ./interaction ./data/data_$j.txt 1000`
+		echo "$(($j*$j)) $duree" >> ./perf/perf_$i.txt 
+	    echo "$j iter"
     done
     echo "Fin: $i procs\n"
 done
