56778cb1db50d44a600572694d218cb8570f2981
Hartorn <bazirehoussin@gmail.com>
2013-12-03 02:05:33 +0100

    analyse des perfs

--- TP3/perf.sh
+++ TP3/perf.sh
@@ -4,9 +4,10 @@ for i in 48 40 30 20 10 1
 do
 	for j in 80 75 70 75 60 55 50 45 40 35 30 25 20 15 10 5 1
 	do
-		genere_data j
+		./genere_data $j
 		duree=`/usr/bin/time -f "%e" mpiexec -np $i ./interaction ./data/data_$j.txt 1000`
-		echo "$j*$j $duree" >> ./perf/perf_$i.txt 
-		#~ echo "Sortie sur $file"
+		echo "$($j*$j) $duree" >> ./perf/perf_$i.txt 
+	    echo "$j iter\n"
     done
+    echo "Fin: $i procs\n"
 done
