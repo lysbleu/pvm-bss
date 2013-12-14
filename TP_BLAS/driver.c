175d51c002856bde3d860c5cbb579cd6c91c5710
svaglica <sylvain.vaglica@gmail.com>
2013-12-14 21:55:15 +0100

    fin test perf ddot

--- TP_BLAS/driver.c
+++ TP_BLAS/driver.c
@@ -38,26 +38,58 @@ int main(int argc, char* argv[])
 	affiche(5,5,matriceC, 5, stdout);
 
 // Tests de performances de ddot	
+	int size = 5;
+	int size2 = size*size;
 
 	blas_t *matriceD, *matriceE;
-	alloc_matrice(&matriceD, 100,100);
-	alloc_matrice(&matriceE, 100,100);
+	alloc_matrice(&matriceD, size, size);
+	alloc_matrice(&matriceE, size, size);
 
 	printf("Tests de performance de la fonction ddot\n");
 	perf_t *t1, *t2;
 	t1 = malloc(sizeof(perf_t));
 	t2 = malloc(sizeof(perf_t));
 	double mflops;
-	perf(t1);
-	blas_t res = ddot(10000, matriceD, 1, matriceE, 1);
-	printf("Résultat du produit scalaire: %le \n", res);
-	perf(t2);
-	perf_printh(t1);
-	perf_printh(t2);
-	perf_diff(t1, t2);
-	perf_printh(t2);
+	char command[200];
+
+	for(size = 5; size2 < 10000000; size += size *25/100)
+	{
+		printf("size2: %d\n", size2);
+		if(size != 5)
+		{
+			free(matriceD);
+			free(matriceE);
+			alloc_matrice(&matriceD, size, size);
+			alloc_matrice(&matriceE, size, size);
+		}
+		size2 = size * size;
+		perf(t1);
+		blas_t res = ddot(size2, matriceD, 1, matriceE, 1);
+		perf(t2);
+		perf_diff(t1, t2);
+		mflops = perf_mflops(t2, size2);
+		long time = t2->tv_usec + ( t2->tv_sec * 1000000);
+		if(size2==25)//ecrasement si fichier existe deja
+		{
+			sprintf(command, "echo %d %ld > results/ddot_perf.txt", size2, time);	
+			system(command);
+		}
+		else//concatenation des donnees
+		{
+			sprintf(command, "echo %d %ld >> results/ddot_perf.txt", size2, time);	
+			system(command);	
+		}
+		
+	}
+	//~ perf(t1);
+	//~ blas_t res = ddot(10000, matriceD, 1, matriceE, 1);
+	//~ printf("Résultat du produit scalaire: %le \n", res);
+	//~ perf(t2);
+	//~ perf_printh(t1);
+	//~ perf_printh(t2);
 	
-	mflops = perf_mflops(t2, 10000);
+	//perf_printh(t2);
+	
 	printf("Mflops/s: %le\n", mflops);
 
 	free(matriceA);
