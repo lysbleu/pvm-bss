75e1dbedf38b5b030d0cbe7d1066b0579ec80180
Hartorn <bazirehoussin@gmail.com>
2014-01-09 19:29:21 +0100

    conflits

--- TP_BLAS/dgemm.c
+++ TP_BLAS/dgemm.c
@@ -47,7 +47,6 @@ void cblas_dgemm_scalaire3(blas_t *C,int ldc, blas_t *A,int lda,  blas_t *B,
     }
 }
 
-<<<<<<< HEAD
 void cblas_dgemm_scalaire(const enum CBLAS_TRANSPOSE TransA,
                  const enum CBLAS_TRANSPOSE TransB, const int M, const int N,
                  const int K, const blas_t alpha, const blas_t *A,
@@ -87,25 +86,6 @@ void cblas_dgemm_scalaire(const enum CBLAS_TRANSPOSE TransA,
 			}
 		}
 	}
-=======
-void cblas_dgemm_scalaire(const int M, const int N,
-                          const int K, const blas_t alpha, const blas_t *A,
-                          const int lda, const blas_t *B, const int ldb,
-                          const blas_t beta, blas_t *C, const int ldc)
-{
-    int i,j,k;
-    for (j=0; j<N; j++)
-    {
-        for (i=0; i<M;i++)
-        {
-            C[j*ldc+i]*=beta;
-            for(k=0; k<K; k++)
-            {	
-                C[j*ldc+i] += alpha*A[k*lda+i]*B[k+j*ldb];
-            }
-        }
-    }
->>>>>>> 30077d0d8b6a29aa79a5902b2482df0d540233cc
 }
 
 void cblas_dgemv(const enum CBLAS_ORDER order,
@@ -114,7 +94,6 @@ void cblas_dgemv(const enum CBLAS_ORDER order,
                  const blas_t *X, const int incX, const blas_t beta,
                  blas_t *Y, const int incY)
 {
-<<<<<<< HEAD
 	int i,j,k;
 	if(order == CblasColMajor)
 	{
@@ -135,7 +114,6 @@ void cblas_dgemv(const enum CBLAS_ORDER order,
 	{
 		printf("cblas_dgemv : Only CblasColMajor is allowed\n"); 
 	}
-=======
     int i,j,k;
     for (j=0; j<N; j++)
     {
@@ -149,7 +127,6 @@ void cblas_dgemv(const enum CBLAS_ORDER order,
             Y[j*incY]*=alpha*beta;
         }
     }
->>>>>>> 30077d0d8b6a29aa79a5902b2482df0d540233cc
 }
 
 void cblas_daxpy(const int N, const blas_t alpha,const blas_t *X, const int ldx,
@@ -166,7 +143,6 @@ void cblas_dger(const enum CBLAS_ORDER order, const int M, const int N,
                 const blas_t alpha, const blas_t *X, const int incX,
                 const blas_t *Y, const int incY, blas_t *A, const int lda)
 {
-<<<<<<< HEAD
 	int i,j;
 	if(order == CblasColMajor)
 	{
@@ -182,16 +158,6 @@ void cblas_dger(const enum CBLAS_ORDER order, const int M, const int N,
 	{
 		printf("cblas_dger : Only CblasColMajor is allowed\n"); 
 	}
-=======
-    int i,j;
-    for(i=0; i< N; i++)
-    {
-        for(j=0; j<M; j++)
-        {
-            A[lda*j+i]+=alpha*X[incX*i]*Y[incY*j];
-        }
-    }
->>>>>>> 30077d0d8b6a29aa79a5902b2482df0d540233cc
 }
 
 void cblas_dgemm(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA,
@@ -200,7 +166,6 @@ void cblas_dgemm(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA
                  const int lda, const blas_t *B, const int ldb,
                  const blas_t beta, blas_t *C, const int ldc)
 {
-<<<<<<< HEAD
 	blas_t *Abis=A;
 	blas_t *Bbis=B;
 	int i,j,A2,A3,A4,B2,B3,B4,lda_bis, ldb_bis;
@@ -285,25 +250,4 @@ void cblas_dgemm(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA
 	{
 		printf("cblas_dgemm : Only CblasColMajor is allowed\n"); 
 	}
-=======
-    cblas_dgemm_scalaire(M/2, N/2, K/2, alpha, A, lda, B, ldb, beta, C, ldc);
-    cblas_dgemm_scalaire(M/2, N/2, K/2, alpha, &(A[(K/2)*lda]), lda, &(B[(K/2)]),
-                         ldb, 1, C, ldc);
-
-    cblas_dgemm_scalaire(M/2, N/2, K/2, alpha, A, lda, &(B[(N/2)*ldb]), ldb,
-                         beta, &(C[(N/2)*ldc]), ldc);
-    cblas_dgemm_scalaire(M/2, N/2, K/2, alpha, &(A[(K/2)*lda]), lda,
-                         &(B[(K/2)+(N/2)*ldb]), ldb, 1,  &(C[(N/2)*ldc]), ldc);
-
-    cblas_dgemm_scalaire(M/2, N/2, K/2, alpha, &(A[(M/2)]), lda, B, ldb, beta,
-                         &(C[M/2]), ldc);
-    cblas_dgemm_scalaire(M/2, N/2, K/2, alpha, &(A[(K/2)*lda+(M/2)]), lda,
-                         &(B[(K/2)]), ldb, 1,  &(C[M/2]), ldc);
-
-    cblas_dgemm_scalaire(M/2, N/2, K/2, alpha, &(A[(M/2)]), lda, &(B[(N/2)*ldb]),
-                         ldb, beta, &(C[M/2+(N/2)*ldc]), ldc);
-    cblas_dgemm_scalaire(M/2, N/2, K/2, alpha, &(A[(K/2)*lda+(M/2)]), lda,
-                         &(B[(K/2)+(N/2)*ldb]), ldb, 1,  &(C[M/2+(N/2)*ldc]),
-                         ldc);
->>>>>>> 30077d0d8b6a29aa79a5902b2482df0d540233cc
 }
