e1174d6b22121ce67d2c2e86feccb167a0c2d2cc
Hartorn <bazirehoussin@gmail.com>
2014-01-09 16:47:58 +0100

    respect de l'interface

--- TP_BLAS/dgemm.h
+++ TP_BLAS/dgemm.h
@@ -4,9 +4,10 @@
 void cblas_dgemm_scalaire1(blas_t *C,int ldc, blas_t *A,int lda,  blas_t *B, int ldb, int m);
 void cblas_dgemm_scalaire2(blas_t *C,int ldc, blas_t *A,int lda,  blas_t *B, int ldb, int m);
 void cblas_dgemm_scalaire3(blas_t *C,int ldc, blas_t *A,int lda,  blas_t *B, int ldb, int m);
-void cblas_dgemm_scalaire(const int M, const int N, const int K, const blas_t alpha, const blas_t *A, const int lda, const blas_t *B, const int ldb, const double beta, blas_t *C, const int ldc);
-void cblas_daxpy_scalaire(const int N, const blas_t alpha,const blas_t *X, const int ldx, /*@out@*/ blas_t *Y, const int ldy);
-void cblas_dgemv_scalaire(const int M, const int N,	const blas_t alpha, const blas_t *A, const int lda, const blas_t *X, const int incX, const blas_t beta,blas_t *Y, const int incY);
-void cblas_dger(const int M, const int N, const double alpha, const double *X, const int incX, const double *Y, const int incY, double *A, const int lda);
-void cblas_dgemm(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA, const enum CBLAS_TRANSPOSE TransB, const int M, const int N, const int K, const double alpha, const double *A, const int lda, const double *B, const int ldb, const double beta, double *C, const int ldc);
-                 #endif /*DGEMM_H_*/
+void cblas_dgemm_scalaire(const int M, const int N, const int K, const blas_t alpha, const blas_t *A, const int lda, const blas_t *B, const int ldb, const blas_t beta, blas_t *C, const int ldc);
+void cblas_daxpy(const int N, const blas_t alpha,const blas_t *X, const int ldx, /*@out@*/ blas_t *Y, const int ldy);
+void cblas_dgemv(const enum CBLAS_ORDER order,
+                 const enum CBLAS_TRANSPOSE TransA,const int M, const int N,	const blas_t alpha, const blas_t *A, const int lda, const blas_t *X, const int incX, const blas_t beta,blas_t *Y, const int incY);
+void cblas_dger(const enum CBLAS_ORDER order,const int M, const int N, const blas_t alpha, const blas_t *X, const int incX, const blas_t *Y, const int incY, blas_t *A, const int lda);
+void cblas_dgemm(const enum CBLAS_ORDER Order, const enum CBLAS_TRANSPOSE TransA, const enum CBLAS_TRANSPOSE TransB,const int M, const int N, const int K, const blas_t alpha, const blas_t *A,const int lda, const blas_t *B, const int ldb, const blas_t beta, blas_t *C, const int ldc);
+#endif /*DGEMM_H_*/
