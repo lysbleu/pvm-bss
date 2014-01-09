56f2c446a3a91f7a598bbeb3fba9589de47b631a
Hartorn <bazirehoussin@gmail.com>
2014-01-09 21:22:53 +0100

    corrections

--- TP_BLAS/dgemm.h
+++ TP_BLAS/dgemm.h
@@ -1,6 +1,34 @@
 #ifndef DGEMM_H_
 #define DGEMM_H_
 #include "util.h"
+
+enum  	CBLAS_ORDER { CblasRowMajor = 101, CblasColMajor = 102 };
+enum  	CBLAS_TRANSPOSE { CblasNoTrans = 111, CblasTrans = 112, CblasConjTrans = 113 };
+enum  	CBLAS_UPLO { CblasUpper = 121, CblasLower = 122 };
+enum  	CBLAS_DIAG { CblasNonUnit = 131, CblasUnit = 132 };
+enum  	CBLAS_SIDE { CblasLeft = 141, CblasRight = 142 };
+
+struct arg{
+	enum CBLAS_TRANSPOSE TransA;
+	enum CBLAS_TRANSPOSE TransB;
+	int M;
+	int N;
+	int K;
+	int alpha;
+	blas_t *A;
+	blas_t lda;
+	blas_t *B;
+	int ldb;
+	blas_t beta;
+	blas_t *C; 
+	int ldc;
+	int A_i1;
+	int A_i2;
+	int B_i1;
+	int B_i2;
+	int C_i;
+};
+
 void cblas_dgemm_scalaire1(blas_t *C,int ldc, blas_t *A,int lda,  blas_t *B, int ldb, int m);
 void cblas_dgemm_scalaire2(blas_t *C,int ldc, blas_t *A,int lda,  blas_t *B, int ldb, int m);
 void cblas_dgemm_scalaire3(blas_t *C,int ldc, blas_t *A,int lda,  blas_t *B, int ldb, int m);
--- TP_BLAS/util.h
+++ TP_BLAS/util.h
@@ -6,14 +6,8 @@
 typedef double blas_t;
 #define CBLAS_INDEX   size_t
 
-#ifndef __INTEL_MKL__
-enum  	CBLAS_ORDER { CblasRowMajor = 101, CblasColMajor = 102 };
-enum  	CBLAS_TRANSPOSE { CblasNoTrans = 111, CblasTrans = 112, CblasConjTrans = 113 };
-enum  	CBLAS_UPLO { CblasUpper = 121, CblasLower = 122 };
-enum  	CBLAS_DIAG { CblasNonUnit = 131, CblasUnit = 132 };
-enum  	CBLAS_SIDE { CblasLeft = 141, CblasRight = 142 };
-
-#endif
+//~ #ifndef __INTEL_MKL__
+//~ #endif
 
 void affiche(int m, int n, blas_t *a, int lda, FILE* flux);
 void alloc_vecteur(blas_t **vecteur, int taille);
