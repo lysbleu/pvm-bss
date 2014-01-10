e90f347c5343a2f0256df834ed530e6835d38c23
svaglica <sylvain.vaglica@gmail.com>
2014-01-10 14:48:25 +0100

    plot

--- TP_BLAS/results/create_plot_dgemm.sh
+++ TP_BLAS/results/create_plot_dgemm.sh
@@ -1,28 +1,28 @@
 #!/usr/bin/bash
 command -v sponge >/dev/null 2>&1 || { echo >&2 "Il faut installer moreutils" ; exit 1; }
 command -v gnuplot >/dev/null 2>&1 || { echo >&2 "Il faut installer gnuplot"; exit 1; }
-echo "# Fichier temporaire pour gnuplot" > plot
-#~ echo "set logscale y" >> plot
-echo "set size ratio -1" >> plot
-#~ echo "set size square" >> plot
-echo "set terminal pngcairo enhanced size 1000,650" >> plot
-echo "set output 'plot.png'" >> plot
-#~ echo "set key on center rmargin box title \"Nombre de\n processus\"" >> plot
-echo "set xlabel \"Temps d'éxéction du produit scalaire (us)\"" >> plot
-echo "set ylabel \"Taille des vecteurs\"" >> plot
+echo "# Fichier temporaire pour gnuplot" > plot_ddot
+#~ echo "set logscale y" >> plot_ddot
+echo "set size ratio -1" >> plot_ddot
+#~ echo "set size square" >> plot_ddot
+echo "set terminal pngcairo enhanced size 1000,650" >> plot_ddot
+echo "set output 'plot_ddot.png'" >> plot_ddot
+#~ echo "set key on center rmargin box title \"Nombre de\n processus\"" >> plot_ddot
+echo "set xlabel \"Temps d'éxéction du produit matriciel (us)\"" >> plot_ddot
+echo "set ylabel \"Taille des matrices (m * n)\"" >> plot_ddot
 
 ###########################################################################
-prefix="perf_"
-values=("ddot")
+prefix="perf_dgemm"
+values=("")
 suffix=".txt"
 ###########################################################################
 
-echo -n "plot " >> plot
+echo -n "plot " >> plot_ddot
 for i in *.txt
 do
-    echo "\"$i\" using 1:2 title \"$i\" with lines,\\" >> plot
+    echo "\"$i\" using 1:2 title \"$i\" with lines,\\" >> plot_ddot
 done
 
-head -c-3 plot | sponge plot
-gnuplot plot
-echo "Sortie sur plot.png"
+head -c-3 plot_ddot | sponge plot_ddot
+gnuplot plot_ddot
+echo "Sortie sur plot_ddot.png"
