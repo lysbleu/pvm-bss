7ddf10f02cfb4293c723aed8b601ae3fa62de9ae
lysbleu <scastelli@ipb.fr>
2014-01-17 01:13:23 +0100

    mpi a l'arrache

--- TP_BLAS/type_mpi.c
+++ TP_BLAS/type_mpi.c
@@ -5,8 +5,8 @@
 
 int main(int argc, char ** argv)
 {
-	MPI_Init(NULL, NULL);
-	int myrank, nb_procs;
+    MPI_Init(NULL, NULL);
+    int myrank, nb_procs;
     MPI_Comm_size( MPI_COMM_WORLD, &nb_procs);
     MPI_Comm_rank( MPI_COMM_WORLD, &myrank ); 
     
@@ -19,16 +19,16 @@ int main(int argc, char ** argv)
     int block_nb_col = 2; //20;
     int block_size = block_nb_col * m;
     int nb_blocks = n /block_nb_col;
-
-	alloc_matrice(&matrice, m, n);
-	alloc_matrice(&recv_mat, m, n / nb_procs);
-	
-	memset(recv_mat, 0, m * nb_blocks * sizeof(blas_t));
-	
+    
+    alloc_matrice(&matrice, m, n);
+    alloc_matrice(&recv_mat, m, n / nb_procs);
+    
+    memset(recv_mat, 0, (m * n / nb_procs) * sizeof(blas_t));
+    
     MPI_Datatype object;
     MPI_Aint stride = m * nb_procs * block_nb_col;
-	MPI_Type_vector( 1, m * block_nb_col , 0, MPI_DOUBLE, &object);
-	MPI_Type_commit(&object);
+    MPI_Type_vector( block_nb_col, m , m, MPI_DOUBLE, &object);
+    MPI_Type_commit(&object);
 	
 	MPI_Status status;
 	
