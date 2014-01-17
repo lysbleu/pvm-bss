f4d1dcc89f150319f2abbdfc91831b6ea39fc144
Hartorn <bazirehoussin@gmail.com>
2014-01-17 14:23:01 +0100

    ajout de LancerRayons

---
+++ LancerRayons/include/const.h
@@ -0,0 +1,43 @@
+/***************************************************************************
+ *                       Lanceur de rayons simplifie                       *
+ ***************************************************************************
+ * ENSERB, 3me annee Informatique                               1995-1996  *
+ * Option AAMP, Module MP2                                                 *
+ *                                                                         *
+ ***************************************************************************
+ * Author : Gilles Subrenat                                                *
+ * Date   : 15 janvier 1996                                                *
+ *                                                                         *
+ * $Id$
+ ***************************************************************************/
+
+#ifndef CONST_H
+#define CONST_H
+
+
+#define	TRUE		1
+#define	FALSE		0
+
+#define PI		(REAL) 3.141592653589793
+#define	RAD		(REAL) 1.745329251994329E-2
+#define	LOG		(REAL) -0.69314718
+#define	INFINITY	(REAL) 1E35
+#define	EPSILON		(REAL) 0.0001
+/*
+**  CONST.H :
+**
+**    Constants definition
+*/
+
+#define	TRUE		1
+#define	FALSE		0
+
+#define PI		(REAL) 3.141592653589793
+#define	RAD		(REAL) 1.745329251994329E-2
+#define	LOG		(REAL) -0.69314718
+#define	INFINITY	(REAL) 1E35
+#define	EPSILON		(REAL) 0.0001
+
+
+
+#endif
