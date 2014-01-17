cd2340faeec564e3ccd4a3ed509e82e374cc9943
Hartorn <bazirehoussin@gmail.com>
2014-01-17 02:23:28 +0100

    code corrigé

--- TP_BLAS/facto_lu.c
+++ TP_BLAS/facto_lu.c
@@ -2,7 +2,48 @@
 #include "facto_lu.h"
 #include <math.h>
 #define MIN(x,y) ((x<y)?x:y)
+#define MAX(x,y) ((x>y)?x:y)
 #define BLOCKSIZE 20
+
+void swap_range(const int M, blas_t *A, const int lda, const int K1, const int K2, int * ipiv)
+{
+	int i;
+	for (i=K1; i<=K2; i++)
+	{
+		if(i != ipiv[i])
+		{
+			swap(M, &(A[i]), &(A[ipiv[i]]), lda);
+		}
+	}
+}
+ 
+void swap(const int M, blas_t *A, blas_t *B, const int lda)
+{
+	int i;
+	blas_t tmp;
+	for (i=0; i<M; i++)
+	{
+		tmp = A[i*lda];
+		A[i*lda] = B[i*lda];
+		B[i*lda] = tmp; 
+	}
+}
+
+int find_id_pivot(const int M, const blas_t *A, const int lda)
+{
+	int fabs_pivot = 0;
+	int id_pivot = 0;
+	int i;
+	for( i=0; i<M; i++)
+	{
+		if( fabs(A[i*lda])> fabs_pivot)
+		{
+			fabs_pivot = fabs(A[i*lda]);
+			id_pivot = i;
+		}
+	}
+	return id_pivot;
+}
 
 void prod_tr(blas_t *A, const int M, blas_t *C, const int lda)
 {
@@ -28,7 +69,7 @@ void prod_tr(blas_t *A, const int M, blas_t *C, const int lda)
 blas_t erreur(blas_t *B, blas_t *C, const int M)
 {
 	blas_t erreur = 0;
-	int i,j;
+	int i;
 	for (i=0; i<M*M; i++)
 	{
 		erreur += fabs(B[i]-C[i]);
@@ -52,6 +93,25 @@ void dgetf2_nopiv( const int M, const int N, blas_t *A, const int lda)
     int limite = MIN(M,N)-1;
     for (k=0; k<  limite; k++)
     {
+
+        dscal(&(A[k*lda+k+1]), M-k-1, 1, 1/A[k*lda+k]);
+        cblas_dger(CblasColMajor, M-k-1, N-k-1, -1, &(A[k*lda+k+1]), 1, &(A[(k+1)*lda+k]), lda, &(A[(k+1)*lda+k+1]), lda);
+    }
+}
+
+void dgetf2( const int M, const int N, blas_t *A, const int lda, int *ipiv)
+{
+    int k;
+    int pivot;
+    int limite = MIN(M,N)-1;
+    for (k=0; k<  limite; k++)
+    {		
+		pivot = k+ find_id_pivot(M-k, &(A[k*lda+k]),lda);
+		ipiv[k]= pivot;
+		if(pivot != k)
+		{
+			swap(N, &(A[k]), &(A[pivot]), lda);
+		}
         dscal(&(A[k*lda+k+1]), M-k-1, 1, 1/A[k*lda+k]);
         cblas_dger(CblasColMajor, M-k-1, N-k-1, -1, &(A[k*lda+k+1]), 1, &(A[(k+1)*lda+k]), lda, &(A[(k+1)*lda+k+1]), lda);
     }
@@ -94,7 +154,7 @@ void dtrsm(const enum CBLAS_SIDE side, const enum CBLAS_UPLO uplo,
                     {
                         if(diag == CblasNonUnit)
                         {
-							printf("lf\n",A[k+k*lda]);
+							printf("%lf\n",A[k+k*lda]);
                             B[k+j*ldb] /=A[k+k*lda];
                         }
                         for(i=0;i<k-1; i++)
@@ -122,7 +182,7 @@ void dtrsm(const enum CBLAS_SIDE side, const enum CBLAS_UPLO uplo,
                     {
                         if(diag == CblasNonUnit)
                         {
-							printf("lf\n",A[k+k*lda]);
+							printf("%lf\n",A[k+k*lda]);
                             B[k+j*ldb] /=A[k+k*lda];
                         }
                         for(i=k;i<M; i++)
@@ -220,6 +280,50 @@ int dgetrf_nopiv(const int M,const int N, blas_t* A, const int lda) {
 			if(i + ib < N) {
 				dtrsm(CblasLeft, CblasLower, CblasNoTrans, CblasUnit, ib, N - i - ib, 1, &(A[i * lda + i]), lda, &A[(i + ib) * lda + i], lda);
 
+				if ( i + ib < M) {
+					cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, M - i - ib, N - i - ib, ib, -1, &(A[i * lda + i + ib]), lda, &A[i + (ib+ i) * lda], lda, 1, &A[i + ib + (i+ ib) * lda], lda);
+				}
+			}
+		}
+    }    
+    return 0;
+}
+
+int dgetrf(const int M,const int N, blas_t* A, const int lda, int *ipiv) {
+    int i,ib,j;
+    if(M < 0) {
+        return -1;
+    }
+    else if (N < 0) {
+        return -2;
+    }
+    else if(lda < 1 || lda < M) {
+        return -4;
+    }
+
+    if (M == 0 || N == 0) {
+        return 0;
+    }
+
+    if (MIN(M,N) < BLOCKSIZE ) {
+        dgetf2(M,N,A,lda, ipiv);
+    }
+    else {
+		int lim;
+        for (i = 0 ; i < MIN(M,N); i+=BLOCKSIZE) {
+			 ib = MIN(MIN(M,N)-i, BLOCKSIZE);
+			 dgetf2(M - i, ib, &(A[i*lda +  i]), lda,&(ipiv[i]));
+             lim = MIN(M, i+ib);
+             
+             for(j=i; j<lim; j++)
+             {
+				 ipiv[j]=i+ipiv[j];
+			 }
+            swap_range(i, A, lda, i, i+ib, ipiv);
+
+			if(i + ib < N) {
+				dtrsm(CblasLeft, CblasLower, CblasNoTrans, CblasUnit, ib, N - i - ib, 1, &(A[i * lda + i]), lda, &A[(i + ib) * lda + i], lda);
+			    swap_range(N-i-ib, &(A[(i+ib)*lda]), lda, i, i+ib, ipiv);
 				if ( i + ib < M) {
 					cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, M - i - ib, N - i - ib, ib, -1, &(A[i * lda + i + ib]), lda, &A[i + (ib+ i) * lda], lda, 1, &A[i + ib + (i+ ib) * lda], lda);
 				}
