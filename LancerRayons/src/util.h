331a9075613e9267349a81bda5b667c7050ee017
Hartorn <bazirehoussin@gmail.com>
2014-01-17 18:30:08 +0100

    projet

---
+++ LancerRayons/src/util.h
@@ -0,0 +1,8 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+int nb_carreaux(const int i, const int j, const int taille);
+int choix_N(const int nb_procs, const int nb_tuiles);
+int stocke_carreaux(int ** tab_carreaux, const int i, const int j, const int taille);
+
+#endif /*_UTIL_H_*/
