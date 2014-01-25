f3e9a7574592474e6520b7df08627022317f9fe4
Hartorn <bazirehoussin@gmail.com>
2014-01-25 16:11:42 +0100

    blabla

--- LancerRayons/src/util.c
+++ LancerRayons/src/util.c
@@ -28,9 +28,9 @@ int stocke_carreaux(int ** tab_carreaux, const int i, const int j, const int tai
 	MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
 	
 	int nb_tuiles = nb_carreaux(i,j, taille);
-	int my_nb_tuiles = (nb_tuiles-myrank)/size+((myrank<(nb_tuiles%size))?1:0);
-	int nb_tuiles_precedentes = ((myrank<(nb_tuiles%size))?myrank:(nb_tuiles%size)) + myrank*(nb_tuiles-1)/size;
-	printf("proc %d : my_nb_tuiles prec %d\n", myrank, nb_tuiles_precedentes); 
+	int my_nb_tuiles = nb_tuiles/size+((myrank<(nb_tuiles%size))?1:0);
+	int nb_tuiles_precedentes = ((myrank<(nb_tuiles%size))?myrank:(nb_tuiles%size)) + myrank*(nb_tuiles/size);
+	printf("proc %d : nb_tuiles prec:%d nb_tuiles:%d nb % reste:%d\n", myrank, nb_tuiles_precedentes,my_nb_tuiles,nb_tuiles%size); 
 		
 	*tab_carreaux = calloc(my_nb_tuiles, sizeof(int));
 	N = choix_N(size, nb_tuiles);
